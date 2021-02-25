e2f53b1139a1159562f522b104a94d2663faa502
Nemanja <nemanjarogic93@gmail.com>
2021-02-25 17:41:23 +0100

    Write mutation resolvers

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -4,6 +4,7 @@ const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./schema");
 const { createStore } = require("./utils");
 const resolvers = require("./resolvers");
+const isEmail = require("isemail");
 
 const LaunchAPI = require("./datasources/launch");
 const UserAPI = require("./datasources/user");
@@ -11,7 +12,24 @@ const UserAPI = require("./datasources/user");
 // Configure SQLite database
 const store = createStore();
 
+// The context function defined above is called once for every GraphQL operation
+// that clients send to our server. The return value of this function becomes the
+// context argument that's passed to every resolver that runs as part of that operation.
+// By creating this context object at the beginning of each operation's execution, all of our
+// resolvers can access the details for the logged-in user and perform actions specifically for that user.
+
 const server = new ApolloServer({
+  context: async ({ req }) => {
+    // simple auth check on every request
+    const auth = (req.headers && req.headers.authorization) || "";
+    const email = Buffer.from(auth, "base64").toString("ascii");
+
+    if (!isEmail.validate(email)) return { user: null };
+
+    const users = await store.users.findOrCreate({ where: { email } });
+    const user = (users && users[0]) || null;
+    return { user: { ...user.dataValues } };
+  },
   typeDefs,
   resolvers,
   dataSources: () => ({
