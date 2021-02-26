1affe92150c461075a868935f98800c4bfba7566
Nemanja <nemanjarogic93@gmail.com>
2021-02-26 09:45:41 +0100

    Fetch data with queries

--- start/client/src/pages/launches.tsx
+++ start/client/src/pages/launches.tsx
@@ -1,7 +1,11 @@
-import React, { Fragment, useState }  from 'react';
-import { RouteComponentProps } from '@reach/router';
-import { gql } from '@apollo/client'
+import React, { Fragment, useState } from "react";
+import { RouteComponentProps } from "@reach/router";
+import { gql, useQuery } from "@apollo/client";
+import { LaunchTile, Header, Button, Loading } from "../components";
+import * as GetLaunchListTypes from "./__generated__/GetLaunchList";
 
+// A fragment is useful for defining a set of fields that
+// you can include across multiple queries without rewriting them.
 export const LAUNCH_TILE_DATA = gql`
   fragment LaunchTile on Launch {
     __typename
@@ -18,10 +22,41 @@ export const LAUNCH_TILE_DATA = gql`
   }
 `;
 
+export const GET_LAUNCHES = gql`
+  query GetLaunchList($after: String) {
+    launches(after: $after) {
+      cursor
+      hasMore
+      launches {
+        ...LaunchTile
+      }
+    }
+  }
+  ${LAUNCH_TILE_DATA}
+`;
+
 interface LaunchesProps extends RouteComponentProps {}
 
 const Launches: React.FC<LaunchesProps> = () => {
-  return <div />;
-}
+  const { data, loading, error } = useQuery<
+    GetLaunchListTypes.GetLaunchList,
+    GetLaunchListTypes.GetLaunchListVariables
+  >(GET_LAUNCHES);
+
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR</p>;
+  if (!data) return <p>Not found</p>;
+
+  return (
+    <Fragment>
+      <Header />
+      {data.launches &&
+        data.launches.launches &&
+        data.launches.launches.map((launch: any) => (
+          <LaunchTile key={launch.id} launch={launch} />
+        ))}
+    </Fragment>
+  );
+};
 
 export default Launches;
