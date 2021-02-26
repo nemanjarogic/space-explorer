cd0468786021f372d38c3018831745f2dbc58958
Nemanja <nemanjarogic93@gmail.com>
2021-02-26 10:04:11 +0100

    Display the profile page and customize the fetch policy

--- start/client/src/pages/profile.tsx
+++ start/client/src/pages/profile.tsx
@@ -1,10 +1,53 @@
-import React from 'react';
-import { RouteComponentProps } from '@reach/router';
+import React, { Fragment } from "react";
+import { gql, useQuery } from "@apollo/client";
+
+import { Loading, Header, LaunchTile } from "../components";
+import { LAUNCH_TILE_DATA } from "./launches";
+import { RouteComponentProps } from "@reach/router";
+import * as GetMyTripsTypes from "./__generated__/GetMyTrips";
+
+export const GET_MY_TRIPS = gql`
+  query GetMyTrips {
+    me {
+      id
+      email
+      trips {
+        ...LaunchTile
+      }
+    }
+  }
+  ${LAUNCH_TILE_DATA}
+`;
 
 interface ProfileProps extends RouteComponentProps {}
 
 const Profile: React.FC<ProfileProps> = () => {
-  return <div />;
-}
+  // Slightly stale/cached data is acceptable in many cases, but we
+  // definitely want our user's list of booked trips to be up to date.
+  // To handle this, we've specified a fetch policy
+  // The default policy is cache-first, which means Apollo Client checks
+  // the cache to see if the result is present before making a network request.
+  const { data, loading, error } = useQuery<GetMyTripsTypes.GetMyTrips>(
+    GET_MY_TRIPS,
+    { fetchPolicy: "network-only" }
+  );
+
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR: {error.message}</p>;
+  if (data === undefined) return <p>ERROR</p>;
+
+  return (
+    <Fragment>
+      <Header>My Trips</Header>
+      {data.me && data.me.trips.length ? (
+        data.me.trips.map((launch: any) => (
+          <LaunchTile key={launch.id} launch={launch} />
+        ))
+      ) : (
+        <p>You haven't booked any trips</p>
+      )}
+    </Fragment>
+  );
+};
 
 export default Profile;
