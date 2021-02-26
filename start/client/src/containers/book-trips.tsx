f0404115c0dd71af33a7a66f7267b92711d88368
Nemanja <nemanjarogic93@gmail.com>
2021-02-26 10:41:24 +0100

    Support logout, booking a trip, and canceling an existing trip

--- start/client/src/containers/book-trips.tsx
+++ start/client/src/containers/book-trips.tsx
@@ -1,7 +1,50 @@
-import React from 'react';
+import React from "react";
+import { gql, useMutation } from "@apollo/client";
 
-const BookTrips: React.FC<any> = () => {
-  return <div/>;
-}
+import Button from "../components/button";
+import { cartItemsVar } from "../cache";
+import * as GetCartItemsTypes from "../pages/__generated__/GetCartItems";
+import * as BookTripsTypes from "./__generated__/BookTrips";
+
+export const BOOK_TRIPS = gql`
+  mutation BookTrips($launchIds: [ID]!) {
+    bookTrips(launchIds: $launchIds) {
+      success
+      message
+      launches {
+        id
+        isBooked
+      }
+    }
+  }
+`;
+
+interface BookTripsProps extends GetCartItemsTypes.GetCartItems {}
+
+const BookTrips: React.FC<BookTripsProps> = ({ cartItems }) => {
+  // The mutation requires a list of launchIds for booking,
+  // which it obtains from the user's locally stored cart(passed as a prop).
+  const [bookTrips, { data }] = useMutation<
+    BookTripsTypes.BookTrips,
+    BookTripsTypes.BookTripsVariables
+  >(BOOK_TRIPS, {
+    variables: { launchIds: cartItems },
+  });
+
+  return data && data.bookTrips && !data.bookTrips.success ? (
+    <p data-testid="message">{data.bookTrips.message}</p>
+  ) : (
+    <Button
+      onClick={async () => {
+        await bookTrips();
+        // Clear the user's cart because the trips in the cart have been booked.
+        cartItemsVar([]);
+      }}
+      data-testid="book-button"
+    >
+      Book All
+    </Button>
+  );
+};
 
 export default BookTrips;
--- start/client/src/containers/logout-button.tsx
+++ start/client/src/containers/logout-button.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React from "react";
+import styled from "react-emotion";
+import { useApolloClient } from "@apollo/client";
 
-const LogoutButton: React.FC<any> = () => {
-  return <div/>;
-}
+import { menuItemClassName } from "../components/menu-item";
+import { isLoggedInVar } from "../cache";
+import { ReactComponent as ExitIcon } from "../assets/icons/exit.svg";
+
+const LogoutButton = () => {
+  const client = useApolloClient();
+  return (
+    <StyledButton
+      data-testid="logout-button"
+      onClick={() => {
+        // Evict and garbage-collect the cached user object
+        client.cache.evict({ fieldName: "me" });
+        client.cache.gc();
+
+        // Remove user details from localStorage
+        localStorage.removeItem("token");
+        localStorage.removeItem("userId");
+
+        // Set the logged-in status to false
+        // When the reactive variable's value changes, that change is automatically broadcast
+        // to every query that depends on the variable's value (IS_LOGGED_IN query).
+        // Because of this, when a user clicks the logout button, our isLoggedIn component
+        // updates to display the login screen.
+        isLoggedInVar(false);
+      }}
+    >
+      <ExitIcon />
+      Logout
+    </StyledButton>
+  );
+};
 
 export default LogoutButton;
+
+const StyledButton = styled("button")(menuItemClassName, {
+  background: "none",
+  border: "none",
+  padding: 0,
+});
