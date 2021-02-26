32489a3ca6a1935f86431815ecb4fa2f68fe375a
Nemanja <nemanjarogic93@gmail.com>
2021-02-26 09:31:08 +0100

    Set up Apollo Client

---
+++ start/client/apollo.config.js
@@ -0,0 +1,8 @@
+// Use this file to configure the behavior of both
+// the Apollo VSCode extension and the Apollo CLI.
+module.exports = {
+  client: {
+    name: "Space Explorer [web]",
+    service: "Deployed graph",
+  },
+};
